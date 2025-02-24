08677a7187854632631dfd6c4a23998dbe00021b
Danish Mehmood <traximuser6@gmail.com>
2025-02-24 19:47:07 +0500

    Post create component code modified

--- src/app/components/post-create/post-create.component.ts
+++ src/app/components/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Post } from "../../models/post.model";
 import { PostService } from "../../services/post.service";
 import { Router, RouterLink } from "@angular/router";
--- src/app/components/post-detail-dialog/post-detail-dialog.component.ts
+++ src/app/components/post-detail-dialog/post-detail-dialog.component.ts
@@ -3,9 +3,10 @@ import { Post } from '../../models/post.model';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { MatCard, MatCardActions, MatCardContent, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
-import { MatButton } from '@angular/material/button';
+import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatSlideToggle } from "@angular/material/slide-toggle";
 import { DatePipe } from "@angular/common";
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-post-detail-dialog',
@@ -13,6 +14,8 @@ import { DatePipe } from "@angular/common";
   imports: [
     MatDialogModule,
     MatCard,
+    MatIcon,
+    MatIconButton,
     MatCardTitle,
     MatCardSubtitle,
     MatCardContent,
@@ -21,29 +24,7 @@ import { DatePipe } from "@angular/common";
     MatSlideToggle,
     DatePipe
   ],
-  template: `
-    <mat-dialog-content>
-      <mat-card class="post-detail-card">
-        <mat-card-title>{{ data.post.title }}</mat-card-title>
-        <mat-card-subtitle>Slug: {{ data.post.slug }}</mat-card-subtitle>
-        <mat-card-content>
-          <p><strong>Excerpt:</strong> {{ data.post.excerpt }}</p>
-          <p><strong>Description:</strong> {{ data.post.description }}</p>
-          <p><strong>Published:</strong>
-            <mat-slide-toggle [checked]="data.post.is_published" disabled class="full-width">
-              {{ data.post.is_published ? 'Yes' : 'No' }}
-            </mat-slide-toggle>
-          </p>
-          <p><strong>Created At:</strong> {{ data.post.created_at | date }}</p>
-          <p><strong>Updated At:</strong> {{ data.post.updated_at | date }}</p>
-        </mat-card-content>
-        <mat-card-actions>
-          <button mat-button color="primary" (click)="dialogRef.close()">Close</button>
-          <button mat-button color="accent" (click)="navigateToEdit()">Edit</button>
-        </mat-card-actions>
-      </mat-card>
-    </mat-dialog-content>
-  `,
+  templateUrl: './post-detail-dialog.component.html',
   styleUrls: ['./post-detail-dialog.component.css']
 })
 
